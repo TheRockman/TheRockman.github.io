34795f8c11b2f92d87d812a3b21f54b4f964aa1f
theRockman <skwekamolfk@gmail.com>
2018-03-27 22:06:10 +0200

    revamp

---
+++ app.js
@@ -0,0 +1,68 @@
+var app = angular.module("myApp", []); app.controller("mainCtrl", function($scope) {
+
+  $scope.projects = [
+    {
+      Title: 'About',
+      Desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      Tags: ['Blog', 'About'],
+      Theme: 'dodgerblue',
+      Poster: './img/profilePoster.png',
+      Backdrop: './img/profileBack.png'
+    },
+    {
+      Title: 'Doodles',
+      Desc: 'I draw whenever I have the time: on the train, at home or while waiting for code to compile. I use a simple ball point pen when im on the go or my wacom tablet.',
+      Tags: ['Drawing', 'Making'],
+      Theme: '#F53C6D',
+      Poster: './img/doodlePoster.png',
+      Backdrop: './img/doodleBack.png',
+      Url: 'https://twitter.com/compileanddraw'
+    },
+    {
+      Title: 'Music',
+      Desc: 'If you are observant you will notice that few of my tracks are longer than one minute. I could claim that this is some artistic choice to impose limits on myself. The truth however is simply that the software im using wont allow larger files to be exported.',
+      Tags: ['Music', 'Making'],
+      Theme: '#6C4796',
+      Poster: './img/musicPoster.png',
+      Backdrop: './img/musicBack.png',
+      Url: 'https://soundcloud.com/user-586995580',
+      Devider: true
+    }
+    // ,{
+    //   Title: 'Boilerplate',
+    //   Desc: '',
+    //   Tags: ['Tag'],
+    //   Theme: '',
+    //   Poster: './img/boilerplatePoster.png',
+    //   Backdrop: '.img/boilerplateback.png.',
+    //   Url: ''
+    // }
+  ];
+
+  (function generateID() {
+    for (var i = 0; i < $scope.projects.length; i++) {
+      $scope.projects[i].Id = i;
+    }
+  })();
+
+  $scope.selectedProject = $scope.projects[0];
+
+  $scope.setSelectedProject = function (project) {
+    $scope.selectedProject = project;
+    if (project.Theme) {
+      $scope.updateTheme(project.Theme);
+    }
+  }
+
+  $scope.isSelectedProject = function (project) {
+    if (project.Id === $scope.selectedProject.Id) {
+      return true;
+    }
+  }
+
+  $scope.updateTheme = function (theme) {
+    document.documentElement.style.setProperty('--primary', theme);
+  }
+
+
+});
