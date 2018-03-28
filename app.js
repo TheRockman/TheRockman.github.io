f7fd41f6a37b986f3581e63881945da298b0ea34
TheRockman <skwekamolfk@gmail.com>
2018-03-28 09:41:57 +0200

    pass chrome audit

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       Desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       Tags: ['Blog', 'About'],
       Theme: 'dodgerblue',
-      Poster: './img/profilePoster.png',
+      Poster: './img/profilePoster.webp',
       PosterSmall: './img/musicPoster.png',
-      Backdrop: './img/profileBack.png'
+      Backdrop: './img/profileBack.webp'
     },
     {
       Title: 'Doodles',
       Desc: 'I draw whenever I have the time: on the train, at home or while waiting for code to compile. I use a simple ball point pen when im on the go or my wacom tablet.',
       Tags: ['Drawing', 'Making'],
       Theme: '#F53C6D',
-      Poster: './img/doodlePoster.png',
-      PosterSmall: './img/musicPoster.png',
-      Backdrop: './img/doodleBack.png',
+      Poster: './img/doodlePoster.webp',
+      PosterSmall: './img/musicPoster.webp',
+      Backdrop: './img/doodleBack.webp',
       Url: 'https://twitter.com/compileanddraw'
     },
     {
@@ -25,9 +25,9 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       Desc: 'If you are observant you will notice that few of my tracks are longer than one minute. I could claim that this is some artistic choice to impose limits on myself. The truth however is simply that the software im using wont allow larger files to be exported.',
       Tags: ['Music', 'Making'],
       Theme: '#6C4796',
-      Poster: './img/musicPoster.png',
-      PosterSmall: './img/musicPoster.png',
-      Backdrop: './img/musicBack.png',
+      Poster: './img/musicPoster.webp',
+      PosterSmall: './img/musicPoster.webp',
+      Backdrop: './img/musicBack.webp',
       Url: 'https://soundcloud.com/user-586995580',
       Devider: true
     }
