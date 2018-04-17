76154abbc7de10bca94809d8ce486d0d0c441304
TheRockman <skwekamolfk@gmail.com>
2018-04-17 10:47:51 +0200

    added cards link

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       Desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       Tags: ['Blog', 'About'],
       Theme: 'dodgerblue',
-      Poster: './img/profilePoster.webp',
-      PosterSmall: './img/profilePoster.webp',
-      Backdrop: './img/profileBack.webp'
+      Poster: 'https://picsum.photos/300/400/?random',
+      PosterSmall: '',
+      Backdrop: './img/bg1.jpg'
     },
     {
       Title: 'Doodles',
       Desc: 'I draw whenever I have the time: on the train, at home or while waiting for code to compile. I use a simple ball point pen when im on the go or my wacom tablet.',
       Tags: ['Drawing', 'Making'],
       Theme: '#F53C6D',
-      Poster: './img/doodlePoster.webp',
-      PosterSmall: './img/doodlePoster.webp',
-      Backdrop: './img/doodleBack.webp',
+      Poster: 'https://picsum.photos/300/400.1/?random',
+      PosterSmall: '',
+      Backdrop: './img/bg1.jpg',
       Url: 'https://twitter.com/compileanddraw'
     },
     {
@@ -25,11 +25,22 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       Desc: 'If you are observant you will notice that few of my tracks are longer than one minute. I could claim that this is some artistic choice to impose limits on myself. The truth however is simply that the software im using wont allow larger files to be exported.',
       Tags: ['Music', 'Making'],
       Theme: '#6C4796',
-      Poster: './img/musicPoster.webp',
-      PosterSmall: './img/musicPoster.webp',
-      Backdrop: './img/musicBack.webp',
+      Poster: 'https://picsum.photos/300/400.2/?random',
+      PosterSmall: '',
+      Backdrop: './img/bg1.jpg',
       Url: 'https://soundcloud.com/user-586995580',
       Devider: true
+    },
+    {
+      Title: 'Project 1 - cards',
+      Desc: 'I like card games. So i tried to make a simple one that can be played on the bus.',
+      Tags: ['Code', 'Making'],
+      Theme: '#6C4796',
+      Poster: 'https://picsum.photos/300/400.2/?random',
+      PosterSmall: '',
+      Backdrop: './img/bg1.jpg',
+      Url: './projects/cards/index.html',
+      Devider: false
     }
     // ,{
     //   Title: 'Boilerplate',
