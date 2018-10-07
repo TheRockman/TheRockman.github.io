2590a51f519ad26477ac50ec6f76e9a92ef4f5de
TheRockman <TheRockman@users.noreply.github.com>
2018-10-07 17:51:17 +0200

    fight

--- projects/map/app.js
+++ projects/map/app.js
@@ -1,12 +1,13 @@
-var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", function($scope) {
+var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", function($scope, $timeout) {
 
   $scope.tiles = [];
+  $scope.playerTile = null;
   $scope.life = {
     player: 10,
     monster: 3
   };
   $scope.spawn = false;
-  
+
   (function init() {
     for (var i = 0; i < 400; i++) {
       if (i == 205) {
@@ -17,7 +18,7 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
             player: true,
             monster: null
           }
-        )  
+        )
       } else if (i == 210) {
         $scope.tiles.push(
           {
@@ -26,7 +27,7 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
             player: null,
             monster: true
           }
-        )  
+        )
       } else {
         $scope.tiles.push(
           {
@@ -39,31 +40,34 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
       }
     }
   })();
-  
+
   function preBuild() {
-    var random = Math.floor(Math.random() * 10) + 1;  
+    var random = Math.floor(Math.random() * 10) + 1;
     if (random == 5) {
       return true;
     } else{
       return false
     }
   }
-  
+
   $scope.playerMove = function (item) {
-    $scope.playerTile = null;
     for (var i = 0; i < $scope.tiles.length; i++) {
       if ($scope.tiles[i].player) {
         $scope.playerTile = $scope.tiles[i];
       }
     }
-    
+
+    if (!item) {
+      return;
+    }
+
     if (item.tall) {
       return;
     } else if(item.id == $scope.playerTile.id + 1 || item.id == $scope.playerTile.id - 1 || item.id == $scope.playerTile.id + 20 || item.id == $scope.playerTile.id - 20){
       if (item.monster) {
         combat(item);
       }
-      
+
       for (var i = 0; i < $scope.tiles.length; i++) {
         $scope.tiles[i].player = null;
       }
@@ -71,19 +75,24 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
       moveEnemy();
     }
   }
-  
-  function combat() {
-    console.log('Combat');
-    $scope.life.player = $scope.life.player - 1;
-    $scope.life.monster = $scope.life.monster - 1;
-    
+  $scope.playerMove();
+
+  function combat(item) {
+    $scope.fight = true;
+    $timeout( function(){
+      $scope.fight = false;
+      $scope.life.player--;
+      $scope.life.monster--;
+    }, 2000 );
     if ($scope.life.monster < 1) {
-      $scope.spawn = true;
+      for (var i = 0; i < $scope.tiles.length; i++) {
+        $scope.tiles[i].monster = null;
+      }
     }
   }
-  
+
   function spawn() {
-    var random = Math.floor(Math.random() * 380) + 1;  
+    var random = Math.floor(Math.random() * 399) + 1;
     if (!$scope.tiles[random].tall && !$scope.tiles[random].player) {
       $scope.life.monster = 3;
       $scope.tiles[random].monster = true;
@@ -92,15 +101,15 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
       spawn();
     }
   }
-  
+
   function moveEnemy() {
     if ($scope.spawn) {
        for (var i = 0; i < $scope.tiles.length; i++) {
          $scope.tiles[i].monster = null;
        }
-       spawn();  
+       spawn();
     }
-    
+
     $scope.enemyTile = null;
     for (var i = 0; i < $scope.tiles.length; i++) {
       if ($scope.tiles[i].monster) {
@@ -114,30 +123,30 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
         }
       }
     }
-    
+
     if ($scope.enemyTile) {
       var d = Math.random();
       if (d < 0.2) {
-        if ($scope.enemyTile.id + 20 < 400 && $scope.tiles[$scope.enemyTile.id + 20].tall !== true) {
-          $scope.tiles[$scope.enemyTile.id + 20].monster = true;  
+        if ($scope.enemyTile.id + 20 < 400 && $scope.tiles[$scope.enemyTile.id + 20].tall !== true && $scope.tiles[$scope.enemyTile.id + 20].player !== true) {
+          $scope.tiles[$scope.enemyTile.id + 20].monster = true;
         } else {
           moveEnemy();
         }
       } else if (d < 0.5) {
-        if ($scope.enemyTile.id - 20 > 0  && $scope.tiles[$scope.enemyTile.id - 20].tall !== true) {
-          $scope.tiles[$scope.enemyTile.id - 20].monster = true;  
+        if ($scope.enemyTile.id - 20 > 0  && $scope.tiles[$scope.enemyTile.id - 20].tall !== true && $scope.tiles[$scope.enemyTile.id - 20].player !== true) {
+          $scope.tiles[$scope.enemyTile.id - 20].monster = true;
         } else {
           moveEnemy();
         }
       } else if (d < 0.7) {
-        if ($scope.enemyTile.id + 1 > 0 && $scope.enemyTile.id + 1 < 400  && $scope.tiles[$scope.enemyTile.id + 1].tall !== true) {
-          $scope.tiles[$scope.enemyTile.id + 1].monster = true;  
+        if ($scope.enemyTile.id + 1 > 0 && $scope.enemyTile.id + 1 < 400  && $scope.tiles[$scope.enemyTile.id + 1].tall !== true  && $scope.tiles[$scope.enemyTile.id + 1].player !== true) {
+          $scope.tiles[$scope.enemyTile.id + 1].monster = true;
         } else {
           moveEnemy();
         }
       } else {
-        if ($scope.enemyTile.id - 1 > 0 && $scope.enemyTile.id - 1 < 400 && $scope.tiles[$scope.enemyTile.id - 1].tall !== true) {
-          $scope.tiles[$scope.enemyTile.id - 1].monster = true;  
+        if ($scope.enemyTile.id - 1 > 0 && $scope.enemyTile.id - 1 < 400 && $scope.tiles[$scope.enemyTile.id - 1].tall !== true  && $scope.tiles[$scope.enemyTile.id - 1].player !== true) {
+          $scope.tiles[$scope.enemyTile.id - 1].monster = true;
         } else {
           moveEnemy();
         }
@@ -145,6 +154,11 @@ var app = angular.module("myApp", ['ngAnimate']); app.controller("mainCtrl", fun
       $scope.enemyTile.monster = false;
     } else {
       $scope.tiles[210].monster = true;
+    }
+    for (var i = 0; i < $scope.tiles.length; i++) {
+      if ($scope.tiles[i].player) {
+        $scope.playerTile = $scope.tiles[i];
+      }
     }
   }
 
