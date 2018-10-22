903c5c884e88ea447d0331e6d9e9da08e558fcd8
TheRockman <skwekamolfk@gmail.com>
2018-10-22 16:32:21 +0200

    simpler

--- projects/gates/app.js
+++ projects/gates/app.js
@@ -9,11 +9,18 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
   $scope.steps = 0;
   $scope.action = null;
   $scope.msg = '';
+  $scope.life = 3;
   
   $scope.updateCSS = function (cvar, value, right) {
+    if ($scope.action === fight || $scope.action === force) {
+      return;
+    }
     $scope.msg = '';
     $scope.currentX = value;
     document.documentElement.style.setProperty(cvar, value+'px');    
+    if ($scope.steps > 5) {
+      exausted();
+    }
     if ($scope.item) {
       if (right) {
         $scope.steps++;
@@ -24,7 +31,7 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       }
       
       itemLogic('--obst-pos', temp);  
-    } else if(Math.floor(Math.random() * 6) <= 1) {
+    } else if(dice() <= 1) {
       document.documentElement.style.setProperty('--obst-pos', '-90px');
       $scope.item = getRandomItem();
     }
@@ -39,6 +46,9 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
       if ($scope.item.type === 'enemy') {
         $scope.action = fight;
       }
+      if ($scope.item.type === 'block') {
+        $scope.action = force;
+      }
     } else if (document.documentElement.style.getPropertyValue('--obst-pos') === '720px') {
       resetItem();
     } else {
@@ -47,31 +57,73 @@ var app = angular.module("myApp", []); app.controller("mainCtrl", function($scop
   }
   
   function pickup() {
-    $scope.msg = 'Picked up ' + $scope.item.label;
+    $scope.msg = 'Ate ' + $scope.item.label;
+    $scope.life = $scope.life + 1;
     resetItem();
   }
   
   function fight() {
-    $scope.msg = 'Killed ' + $scope.item.label;
+    if (dice() > 2) {
+      $scope.msg = 'Killed ' + $scope.item.label;
+      resetItem();    
+    } else {
+      $scope.msg = 'Got hit by ' + $scope.item.label;
+      $scope.life = $scope.life - 1;
+    }
+    if ($scope.life < 1) {
+      $scope.life = 1;
+      $scope.msg = "Died"
+    }
+  }
+  
+  function force() {
+    $scope.msg = 'Broke ' + $scope.item.label;
     resetItem();
   }
   
-  function getRandomItem() {
-    var dice = Math.floor(Math.random() * 6);
-    if (dice > 3) {
+  function exausted() {
+    $scope.msg = 'Took damage due to hunger';
+    $scope.life = $scope.life - 1;
+  }
+  
+  $scope.getNumber = function(num) {
+    return new Array(num);   
+}
+  
+  function dice() {
+    return Math.floor(Math.random() * 7);
+  }
+  
+  function getRandomItem() { 
+    var ra = dice();
+    if (ra === 1) {
+      return {
+        class: 'skeleton',
+        actionLabel: 'Punch',
+        type: 'enemy',
+        label: 'the skeleton',
+      }
+    } else if (ra === 2) {
       return {
         class: 'apple',
         actionLabel: 'Pick up',
         type: 'pickup',
         label: '1x apple',
-      };
+      }
+    } else if (ra === 3) {
+      return {
+        class: 'block',
+        actionLabel: 'Break',
+        type: 'block',
+        label: 'crate',
+      }
     } else {
       return {
         class: 'skeleton',
-        actionLabel: 'Hit',
+        actionLabel: 'Punch',
         type: 'enemy',
         label: 'the skeleton',
-      };      
+      };
     }
   }
   
