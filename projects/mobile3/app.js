557a9d6ef293120113510e04c018f5d943cfc4f1
TheRockman <skwekamolfk@gmail.com>
2019-09-12 13:11:59 +0200

    comi

--- projects/mobile3/app.js
+++ projects/mobile3/app.js
@@ -1,3 +1,7 @@
 var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
-
+  $scope.slide = 0;
+  $scope.toggleActive = function(){
+    $scope.active = !$scope.active;
+    console.log('123');
+  }
 });
