5b0482e2f28f85e847e79621a92876e19278e8be
TheRockman <skwekamolfk@gmail.com>
2019-10-18 14:59:25 +0200

    score

--- projects/scoreboard2/app.js
+++ projects/scoreboard2/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
   $scope.challange = function(){
     $scope.duel = true;
-    $scope.search = null;
+    $scope.search = null; 
   }
 });
