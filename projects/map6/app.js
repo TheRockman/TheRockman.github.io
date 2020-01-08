957232afb5343c0039df5e634a85830d60198a84
TheRockman <skwekamolfk@gmail.com>
2020-01-08 14:17:24 +0100

    mmh

--- projects/map6/app.js
+++ projects/map6/app.js
@@ -1,9 +1,9 @@
-var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
+var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope, $timeout) {
   
   $scope.grid = [];
-  $scope.player1Pos;
-  $scope.player2Pos;
-  $scope.player3Pos;
+  $scope.playerPos = {'transform': 'translate(0px, 0px) translateZ(4px) scale(1) rotateZ(-45deg) rotateX(16deg)'};
+  $scope.playerCord = {x:0, y:0};
+  $scope.prevDir;
   
   generateTiles = function(){
     var gridSize = 12;
@@ -20,7 +20,8 @@ var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controll
         
         $scope.grid.push({
           x: x,
-          y: y
+          y: y,
+          status: null,
         })
         
       }
@@ -28,24 +29,119 @@ var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controll
   }
   generateTiles();
   
-  $scope.moveToTile = function(tile){
+  $scope.grid[100].status = 'blocked';
+  $scope.grid[88].status = 'blocked';
+  
+  $scope.moveToTile = function(target){
+    explore(target);
+  };
+  
+  $scope.exploreFailedSoTry;
+  
+  var explore = function(target, failed){
+    var west = {x: $scope.playerCord.x-1, y: $scope.playerCord.y};
+    var north = {x: $scope.playerCord.x, y: $scope.playerCord.y-1};
+    var east = {x: $scope.playerCord.x+1, y: $scope.playerCord.y};
+    var south = {x: $scope.playerCord.x, y: $scope.playerCord.y+1};
     
-    if($scope.current === 'player1'){
-      $scope.player1Pos = {
-        'transform': 'translate(' + tile.x*16 + 'px, ' + tile.y*16 + 'px) translateZ(4px) scale(1) rotateZ(-45deg) rotateX(16deg)',
-        'z-index': tile.y+1
+    if ($scope.exploreFailedSoTry) {
+      $scope.exploreFailedSoTry = null;
+      
+        if(target.y < $scope.playerCord.y){
+          if(checkLocation(west)){
+            $scope.walk(west);
+            $scope.dir = 'south';
+          } 
+        } else if(target.y > $scope.playerCord.y){
+          if(checkLocation(east)){
+            $scope.walk(east);
+            $scope.dir = 'north';
+          }
+        } else if(target.x < $scope.playerCord.x){
+          if(checkLocation(north)){
+            $scope.walk(north);
+            $scope.dir = 'east';
+          }
+        } else if(target.x > $scope.playerCord.x){
+          if(checkLocation(south)){
+            $scope.walk(south);
+            $scope.dir = 'west';
+          }
+        }
+        if(target.x != $scope.playerCord.x || target.y != $scope.playerCord.y){
+          $timeout( function(){
+            explore(target)
+          }, 500 );
+        }
+        return;
+    }
+    
+      if(target.x < $scope.playerCord.x){
+        if(checkLocation(west)){
+          $scope.walk(west);
+          $scope.dir = 'west';
+        } else{
+          $scope.exploreFailedSoTry = east;
+        }
+      } else if(target.x > $scope.playerCord.x){
+        if(checkLocation(east)){
+          $scope.walk(east);
+          $scope.dir = 'east';
+        } else {
+          $scope.exploreFailedSoTry = west;
+        }
+      } else if(target.y > $scope.playerCord.y){
+        if(checkLocation(south)){
+          $scope.walk(south);
+          $scope.dir = 'south';
+        } else{
+          $scope.exploreFailedSoTry = north;
+        }
+      } else if(target.y < $scope.playerCord.y){
+        if(checkLocation(north)){
+          $scope.walk(north);
+          $scope.dir = 'north';
+        } else{
+          $scope.exploreFailedSoTry = south;
+        }
       }
-    } else if($scope.current === 'player2') {
-      $scope.player2Pos = {
-        'transform': 'translate(' + tile.x*16 + 'px, ' + tile.y*16 + 'px) translateZ(4px) scale(1) rotateZ(-45deg) rotateX(16deg)',
-        'z-index': tile.y+1
+      
+      if(target.x != $scope.playerCord.x || target.y != $scope.playerCord.y){
+        $timeout( function(){
+          explore(target)
+        }, 500 );
       }
-    } else {
-      $scope.player3Pos = {
-        'transform': 'translate(' + tile.x*16 + 'px, ' + tile.y*16 + 'px) translateZ(4px) scale(1) rotateZ(-45deg) rotateX(16deg)',
-        'z-index': tile.y+1
+    
+    
+  }
+  
+  var checkLocation = function(location){
+    var targetTile;
+    
+    for (i = 0; i < $scope.grid.length; i++) {
+      if(location.x === $scope.grid[i].x && location.y === $scope.grid[i].y){
+        var targetTile = $scope.grid[i];
       }
     }
-
+    
+    if(targetTile.status === 'blocked'){
+      return false;
+    }
+    if(targetTile.x < 0 && targetTile.x > gridSize){
+      return false;
+    }
+    if(targetTile.y < 0 && targetTile.y > gridSize){
+      return false;
+    }
+    return true;
   }
+  
+  $scope.walk = function(dir){
+    console.log('dir',dir);
+    $scope.playerPos = {
+      'transform': 'translate(' + dir.x*16 + 'px, ' + dir.y*16 + 'px) translateZ(4px) scale(1) rotateZ(-45deg) rotateX(16deg)'
+    }
+    $scope.playerCord = {x:dir.x, y:dir.y};
+  }
+  
 });
