965c38b9fb904ff418c542791ffadfc61987e1ba
TheRockman <skwekamolfk@gmail.com>
2020-06-18 14:06:20 +0200

    mobile 11

---
+++ projects/mobile11/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", function($scope) {
+  $scope.view = 1;
+});
+
+// Extra modules
+// var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
+//
+// });
