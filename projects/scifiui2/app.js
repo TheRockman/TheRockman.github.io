520313b3d228a79d71d3e942e137ae52b596da5f
TheRockman <skwekamolfk@gmail.com>
2020-10-16 12:17:50 +0200

    ui

--- projects/scifiui2/app.js
+++ projects/scifiui2/app.js
@@ -1,8 +1,15 @@
 var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", function($scope) {
 
+  $scope.x = 550;
+  $scope.y = 0;
+
+  $scope.ctrlMode = false;
+
   $scope.captureCoordinate = function($event){
-     $scope.x = $event.x;
-     $scope.y = $event.y;
+    if($scope.ctrlMode){
+      $scope.x = $event.x;
+      $scope.y = $event.y;
+    }
   }
 
 });
