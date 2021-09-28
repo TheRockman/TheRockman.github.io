1f2d04e864575046240e3dd0d089158c3eca14e3
theRockman <skwekamolfk@gmail.com>
2021-09-28 09:53:13 +0200

    dash6

---
+++ projects/dashboard6/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", function($scope) {
+    $scope.openNav = false;
+  
+});
+
+// Extra modules
+// var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
+//
+// });
