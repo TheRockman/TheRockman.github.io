4f37864e2a70e54284bb7efe14f234ab1a41ebc1
Rockman <skwekamolfk@gmail.com>
2021-10-15 11:09:03 +0200

    modern shade

--- projects/boilerplate/app.js
+++ projects/boilerplate/app.js
@@ -1,8 +1,4 @@
 var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", function($scope) {
 
 });
-
-// Extra modules
-// var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
-//
-// });
+//['ngTouch', 'angular-carousel']
