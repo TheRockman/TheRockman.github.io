e40898c81ce62ddc2c1832bbfdddf3d4c673b823
Rockman <skwekamolfk@gmail.com>
2021-10-18 15:36:09 +0200

    snazzy

--- projects/calendar/app.js
+++ projects/calendar/app.js
@@ -63,13 +63,15 @@ var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", funct
     return calendarArr;
   }
 
-  $scope.displayYear = 2021;
+  let today = new Date();
+
+  $scope.displayYear = today.getFullYear();
   $scope.setDisplayYear = function(mod){
     $scope.displayYear = $scope.displayYear + mod;
     $scope.cal = $scope.generateCalendar($scope.displayYear);
   }
 
-  $scope.displayMonth = 1;
+  $scope.displayMonth = today.getMonth()+1;
   $scope.setDisplayMonth = function(mod){
     $scope.displayMonth = $scope.displayMonth + mod;
     if($scope.displayMonth + mod > 12){
@@ -88,6 +90,7 @@ var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", funct
 
   $scope.cal = $scope.generateCalendar($scope.displayYear);
 
-  $scope.cal[1][20].events = [{title: 'Mikes birthday', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}]
+  $scope.m = ['','Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  $scope.cal[$scope.displayMonth][20].events = [{title: 'Mikes birthday', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}]
 
 });
