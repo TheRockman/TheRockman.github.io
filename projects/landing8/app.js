47ede5d3e8a6c651e904c9a113e7272520d0dc75
Rockman <skwekamolfk@gmail.com>
2021-11-08 10:47:13 +0100

    bleh

--- projects/landing8/app.js
+++ projects/landing8/app.js
@@ -1,8 +1,34 @@
 var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controller("mainCtrl", function($scope) {
-  $scope.index;
-  
-  $scope.theme = function(deg){
-    document.documentElement.style.setProperty('--filter', 'hue-rotate(' + deg + 'deg)');
-  }
+  $scope.index = 0;
 
+  $scope.images = [
+    'https://i0.wp.com/www.solopress.com/blog/wp-content/uploads/2014/10/halo-video-game-poster-by-dylan-west.jpg?resize=388%2C600&ssl=1',
+    'https://i2.wp.com/www.solopress.com/blog/wp-content/uploads/2014/10/zelda-video-game-poster-by-dylan-west.jpg?resize=388%2C600&ssl=1',
+    'https://i2.wp.com/www.solopress.com/blog/wp-content/uploads/2014/10/titanfall-video-game-poster-by-dylan-west.jpg?resize=388%2C600&ssl=1'
+  ]
+
+  $scope.reviews = [
+    {
+      name: 'Bobbo',
+      stars: '⭐ ⭐ ⭐ ⭐ ⭐',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore!',
+      date: '20/12/2020'
+    },
+    {
+      name: 'Bobbo',
+      stars: '⭐ ⭐ ⭐',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore!',
+      date: '20/12/2020'
+    },
+    {
+      name: 'Bobbo',
+      stars: '⭐',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore!',
+      date: '20/12/2020'
+    },
+  ]
+
+  $scope.applyIndex = function(i){
+    $scope.index = i;
+  }
 });
