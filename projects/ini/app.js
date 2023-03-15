51982436177645e6451bf0bb637e6d0509a455a3
Rockman <skwekamolfk@gmail.com>
2023-03-15 16:17:08 +0100

    Revert "spring clean"
    This reverts commit 1500fb178fd8db096c2141bf50f34f0b4896040b.

--- projects/ini/app.js
+++ projects/ini/app.js
@@ -6,9 +6,6 @@ $scope.dice = 'Ready to roll';
 $scope.addToOrder = function () {
   $scope.order.push($scope.form);
   $scope.form = {};
-
-
-    document.getElementById("myTextField").focus();
 }
 
 $scope.clear = function () {
