53f8d2775803a6d093219874038ab3bef59e73c0
Rockman <skwekamolfk@gmail.com>
2025-02-25 18:19:17 +0100

    wip

---
+++ projects/scifiui1/app.js
@@ -0,0 +1,51 @@
+var app = angular.module("myApp", ['ngTouch']); app.controller("mainCtrl", function($scope) {
+
+  $scope.items = [
+    {
+      id: 1,
+      active: false
+    },
+    {
+      id: 2,
+      active: false
+    },
+    {
+      id: 3,
+      active: false
+    },
+    {
+      id: 4,
+      active: false
+    },
+    {
+      id: 5,
+      active: false
+    },
+    {
+      id: 6,
+      active: false
+    },
+    {
+      id: 7,
+      active: false
+    },
+    {
+      id: 8,
+      active: false
+    },
+    {
+      id: 9,
+      active: false
+    },
+    {
+      id: 10,
+      active: false
+    },
+    {
+      id: 11,
+      active: false
+    },
+  ]
+
+});
+//['ngTouch', 'angular-carousel']
