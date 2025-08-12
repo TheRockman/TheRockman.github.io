e150771ca57ac0cd215731f116774a1d5ba32de6
Rockman <skwekamolfk@gmail.com>
2025-08-12 21:35:21 +0200

    ENG NPC

--- projects/ini/app.js
+++ projects/ini/app.js
@@ -22,7 +22,7 @@ var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controll
         output.push({
           hp: null,
           ini: parseInt($scope.pasteDump.split("\n")[i + 1]),
-          name: $scope.pasteDump.split("\n")[i-1]
+          name: $scope.pasteDump.split("\n")[i - 1]
         });
       }
     }
@@ -34,203 +34,310 @@ var app = angular.module("myApp", ['ngTouch', 'angular-carousel']); app.controll
     $scope.pasteDump = undefined;
   }
 
-$scope.clear = function () {
-  $scope.form = {};
-  $scope.order = [];
-}
-$scope.numDice = 1;
+  $scope.clear = function () {
+    $scope.form = {};
+    $scope.order = [];
+  }
+  $scope.numDice = 1;
 
-$scope.roll = function (sides) {
-  $scope.dice = 0;
-  $scope.diceArr = [];
-  $scope.diceTot = 0;
-  var t = 0;
+  $scope.roll = function (sides) {
+    $scope.dice = 0;
+    $scope.diceArr = [];
+    $scope.diceTot = 0;
+    var t = 0;
 
-  if ($scope.numDice > 1) {
-    for (let index = 0; index < $scope.numDice; index++) {
-      let ding = {
-        val: Math.floor((Math.random() * sides) + 1),
-        id: new Date(),
-      };
-      t = t + ding.val;
-      $scope.diceTot = t;
-      $scope.diceArr.push(ding);
+    if ($scope.numDice > 1) {
+      for (let index = 0; index < $scope.numDice; index++) {
+        let ding = {
+          val: Math.floor((Math.random() * sides) + 1),
+          id: new Date(),
+        };
+        t = t + ding.val;
+        $scope.diceTot = t;
+        $scope.diceArr.push(ding);
+      }
+    }
+    else {
+      $scope.dice = Math.floor((Math.random() * sides) + 1);
     }
   }
-  else {
-    $scope.dice = Math.floor((Math.random() * sides) + 1);
+
+  $scope.maleENG = [
+    'ADRIAN',
+    'ABE',
+    'GRYM',
+    'ISS',
+    'TIMOTHY',
+    'MELWIN',
+    'JIM',
+    'SILAS',
+    'HANS',
+    'ZAC',
+    'EMAD',
+    'JON',
+    'ALEC',
+    'AXEL',
+    'JOB',
+    'JAKE',
+    'JOACIM',
+    'ISMAEL',
+    'FILIP',
+    'GEORGE',
+    'MIO',
+    'LAVELLAN',
+    'HORATIO',
+    'LIAS',
+    'HUGO',
+    'PAUL',
+  ]
+
+  $scope.femaleENG = [
+    'ASMA',
+    'IRMA',
+    'FREIJA',
+    'IZABELL',
+    'ALVA',
+    'KENZA',
+    'MARRY',
+    'MEDINA',
+    'STELLA',
+    'EMY',
+    'NINA',
+    'MERVI',
+    'TYRA',
+    'HILD',
+    'ANN',
+    'SUND',
+    'ANJA',
+    'ELLEN',
+    'MADELEINE',
+    'SIV',
+    'LIN',
+    'ISABELLE',
+    'LEE',
+    'AMALIA'
+  ]
+
+  $scope.prefixENG = [
+    'THREAD',
+    'WOLF',
+    'HAND',
+    'BEAK',
+    'FLITTER',
+    'ICE',
+    'SNOW',
+    'OAK',
+    'MARBLE',
+    'COPPER',
+    'CLAW',
+    'STORM',
+    'BEAR',
+    'ORE',
+    'GRIM',
+    'SILVER',
+    'PLOW'
+  ]
+
+  $scope.suffixENG = [
+    'STAR',
+    'BEAM',
+    'STEEL',
+    'LIGHT',
+    'FIST',
+    'SHIELD',
+    'SWORD',
+    'HILT',
+    'SPEAR',
+    'PAW',
+    'GRIP',
+    'STRIDE',
+    'FLOWER',
+    'MARSH',
+    'SSON',
+    'OWL',
+    'HAWK',
+    'EAGLE',
+    'DRAKE',
+  ];
+
+  $scope.maleSE = [
+    'ADRIAN',
+    'ABBE',
+    'TIG',
+    'ISS',
+    'TIMOTHY',
+    'MELWIN',
+    'JIM',
+    'SILAS',
+    'HANS',
+    'ZAC',
+    'EMAD',
+    'JON',
+    'ALEC',
+    'AXEL',
+    'JOB',
+    'FINGAL',
+    'JOACIM',
+    'ISMAEL',
+    'FILIP',
+    'GöSTA',
+    'MIO',
+    'UNO',
+    'TOKE',
+    'LIAS',
+    'HUGO',
+    'PÅL',
+  ]
+
+  $scope.femaleSE = [
+    'ASMA',
+    'IRMA',
+    'FREIJA',
+    'IZABELL',
+    'ALVA',
+    'KENZA',
+    'GERD',
+    'MEDINA',
+    'STELLA',
+    'EMY',
+    'NINA',
+    'MERVI',
+    'TYRA',
+    'TOKA',
+    'ANN',
+    'SUND',
+    'ANJA',
+    'ELLEN',
+    'MADELEINE',
+    'SIV',
+    'LIN',
+    'ISABELLE',
+    'LEE',
+    'AMALIA'
+  ]
+
+  $scope.prefixSE = [
+    'TRÅD',
+    'ULF',
+    'HAND',
+    'NÄBB',
+    'FLYKT',
+    'IS',
+    'SNÖ',
+    'EK',
+    'MARMOR',
+    'KOPPAR',
+    'KLO',
+    'STORM',
+    'BJÖRN',
+    'MALM',
+    'SVÄLT',
+    'SILVER',
+    'ÄNG'
+  ]
+  $scope.suffixSE = [
+    'STJÄRNA',
+    'STRÅLE',
+    'STÅL',
+    'LJUS',
+    'NÄVE',
+    'SKÖLD',
+    'SVÄRD',
+    'BANE',
+    'SPJUT',
+    'TASS',
+    'FAST (e)',
+    'HALT (e)',
+    'VIG (e)',
+    'BLOM',
+    'MYR',
+    'BLIND (e)',
+    'HÄNT (e)',
+    'SON',
+    'UGGLA',
+    'FALK',
+    'ÖRN',
+    'DRAKE',
+    'VAN (e)',
+  ];
+
+  $scope.jobs = [
+    'Mekaniker',
+    'Uppfödare',
+    'Shaman',
+    'glasblsåsare',
+    'Stable hand',
+    'Innkeeper',
+    'Blacksmith',
+    'Washerwoman',
+    'Town cryer',
+    'Tailor',
+    'Portrait painter',
+    'Cobbler',
+    'Lamplighter',
+    'Tanner',
+    'Herbalist',
+    'Farmer',
+    'Carpenter',
+    'Stonemason',
+    'Miner',
+    'Fortune teller',
+    'Baker',
+    'Town guard',
+    'Doctor',
+    'Messenger',
+    'Banker',
+    'Animal tamer',
+    'Guide',
+    'Cartographer',
+    'Academic',
+    'Actor',
+    'Apprentice',
+    'Brewer',
+    'Cook',
+    'Fisher',
+    'Gravedigger',
+    'Lumberjack',
+    'Sailor',
+    'Scribe',
+    'Servant',
+    'Trader',
+    'Wanderer',
+    'Wizard',
+    'Wrestler',
+    'Author',
+    'Clocksmith',
+    'Alchemist',
+    'Librarian',
+    'Woodcarver',
+    'Roofer',
+    'Sculptor'
+  ]
+
+  $scope.gen = function (gender, reg) {
+    var firstName = '';
+    var lastname = '';
+    $scope.npc = null;
+
+    $scope.male = reg === 'SE' ? $scope.maleSE : $scope.maleENG;
+    $scope.female = reg === 'SE' ? $scope.femaleSE : $scope.femaleENG;
+    $scope.prefix = reg === 'SE' ? $scope.prefixSE : $scope.prefixENG;
+    $scope.suffix = reg === 'SE' ? $scope.suffixSE : $scope.suffixENG;
+
+    $scope.work = $scope.jobs[Math.floor((Math.random() * $scope.jobs.length - 1) + 1)];
+    if (gender === 'm') {
+      firstName = $scope.male[Math.floor((Math.random() * $scope.male.length - 1) + 1)]
+    } else if (gender === 'f') {
+      firstName = $scope.female[Math.floor((Math.random() * $scope.female.length - 1) + 1)]
+    }
+    
+    console.log($scope.male, $scope.female, $scope.prefix.length, $scope.suffix.length);
+
+    lastname = $scope.prefix[Math.floor((Math.random() * $scope.prefix.length - 1) + 1)] + '-' + $scope.suffix[Math.floor((Math.random() * $scope.suffix.length - 1) + 1)];
+
+    var fullName = firstName + ' ' + lastname;
+    $scope.npc = fullName;
+
   }
-}
-
-$scope.male = [
-  'ADRIAN',
-  'ABBE',
-  'TIG',
-  'ISS',
-  'TIMOTHY',
-  'MELWIN',
-  'JIM',
-  'SILAS',
-  'HANS',
-  'ZAC',
-  'EMAD',
-  'JON',
-  'ALEC',
-  'AXEL',
-  'JOB',
-  'FINGAL',
-  'JOACIM',
-  'ISMAEL',
-  'FILIP',
-  'GöSTA',
-  'MIO',
-  'UNO',
-  'TOKE',
-  'LIAS',
-  'HUGO',
-  'PÅL',
-]
-
-$scope.female = [
-  'ASMA',
-  'IRMA',
-  'FREIJA',
-  'IZABELL',
-  'ALVA',
-  'KENZA',
-  'GERD',
-  'MEDINA',
-  'STELLA',
-  'EMY',
-  'NINA',
-  'MERVI',
-  'TYRA',
-  'TOKA',
-  'ANN',
-  'SUND',
-  'ANJA',
-  'ELLEN',
-  'MADELEINE',
-  'SIV',
-  'LIN',
-  'ISABELLE',
-  'LEE',
-  'AMALIA'
-]
-
-$scope.prefix = [
-  'TRÅD',
-  'ULF',
-  'HAND',
-  'NÄBB',
-  'FLYKT',
-  'IS',
-  'SNÖ',
-  'EK',
-  'MARMOR',
-  'KOPPAR',
-  'KLO',
-  'STORM',
-  'BJÖRN',
-  'MALM',
-  'SVÄLT',
-  'SILVER',
-  'ÄNG'
-]
-$scope.sufix = [
-  'STJÄRNA',
-  'STRÅLE',
-  'STÅL',
-  'LJUS',
-  'NÄVE',
-  'SKÖLD',
-  'SVÄRD',
-  'BANE',
-  'SPJUT',
-  'TASS',
-  'FAST (e)',
-  'HALT (e)',
-  'VIG (e)',
-  'BLOM',
-  'MYR',
-  'BLIND (e)',
-  'HÄNT (e)',
-  'SON',
-  'UGGLA',
-  'FALK',
-  'ÖRN',
-  'DRAKE',
-  'VAN (e)',
-]
-
-$scope.jobs = [
-  'Mekaniker',
-  'Uppfödare',
-  'Shaman',
-  'glasblsåsare',
-  'Stable hand',
-  'Innkeeper',
-  'Blacksmith',
-  'Washerwoman',
-  'Town cryer',
-  'Tailor',
-  'Portrait painter',
-  'Cobbler',
-  'Lamplighter',
-  'Tanner',
-  'Herbalist',
-  'Farmer',
-  'Carpenter',
-  'Stonemason',
-  'Miner',
-  'Fortune teller',
-  'Baker',
-  'Town guard',
-  'Doctor',
-  'Messenger',
-  'Banker',
-  'Animal tamer',
-  'Guide',
-  'Cartographer',
-  'Academic',
-  'Actor',
-  'Apprentice',
-  'Brewer',
-  'Cook',
-  'Fisher',
-  'Gravedigger',
-  'Lumberjack',
-  'Sailor',
-  'Scribe',
-  'Servant',
-  'Trader',
-  'Wanderer',
-  'Wizard',
-  'Wrestler',
-  'Author',
-  'Clocksmith',
-  'Alchemist',
-  'Librarian',
-  'Woodcarver',
-  'Roofer',
-  'Sculptor'
-]
-
-$scope.gen = function (gender) {
-  var firstName = '';
-  var lastname = '';
-  $scope.work = $scope.jobs[Math.floor((Math.random() * $scope.jobs.length - 1) + 1)];
-  if (gender === 'm') {
-    firstName = $scope.male[Math.floor((Math.random() * $scope.male.length - 1) + 1)]
-  } else if (gender === 'f') {
-    firstName = $scope.female[Math.floor((Math.random() * $scope.female.length - 1) + 1)]
-  }
-
-  lastname = $scope.prefix[Math.floor((Math.random() * $scope.prefix.length - 1) + 1)] + '-' + $scope.sufix[Math.floor((Math.random() * $scope.sufix.length - 1) + 1)];
-
-  var fullName = firstName + ' ' + lastname;
-  $scope.npc = fullName;
-
-}
 
 });
